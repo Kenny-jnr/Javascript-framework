2258686a5de069c08e7291dab184fac55f1b375b
Kenny Umobi <122109136+Kenny-jnr@users.noreply.github.com>
2024-03-04 21:55:10 +0000

    random number generator

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("random", function() {
+    return `
+    <div x-data="{ names: ['Kenny', 'Mike', 'Tom', 'Jerry', 'Nicole', 'Oumy', 'Jenessa', 'Ndiaga', 'Andre', 'Princess'], randomName: '' }">
+      <button @click="randomName = names[Math.floor(Math.random() * names.length)]">Generate Name</button>
+      <h1 x-text="randomName"></h1>
+    </div>
+    `
+  })
 };
